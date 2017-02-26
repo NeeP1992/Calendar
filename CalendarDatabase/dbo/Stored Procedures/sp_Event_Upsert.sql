436db33d4391b224d7c735b5f27cb7d4907a5fcb
NEIL\Neil <neil@neoncubesoftware.com>
2017-02-26 12:16:53 +0000

    Added database to project.

---
+++ CalendarDatabase/dbo/Stored Procedures/sp_Event_Upsert.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [dbo].[sp_Event_Upsert]
+	@Id int output,
+    @Text varchar(500),
+    @EventStart datetime,
+    @EventEnd datetime
+AS
+BEGIN
+		
+	BEGIN
+		IF EXISTS (SELECT * FROM [Event] WHERE Id = @Id)
+			BEGIN
+				UPDATE [Event] SET
+					[Text] = @Text,
+					[EventStart] = @EventStart,
+					[EventEnd] = @EventEnd
+				WHERE Id = @Id
+
+				Select @Id
+			END
+		ELSE
+			BEGIN
+				INSERT INTO [Event]
+				(
+					[Text],
+					[EventStart],
+					[EventEnd]
+				)
+				VALUES
+				(
+					@Text,
+					@EventStart,
+					@EventEnd
+				)
+
+				Select SCOPE_IDENTITY()
+			END
+	END
+
+END
---
+++ CalendarDatabase/dbo/Tables/Event.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Event] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [Text]       VARCHAR (500) NOT NULL,
+    [EventStart] DATETIME      NOT NULL,
+    [EventEnd]   DATETIME      NOT NULL,
+    CONSTRAINT [PK_Event] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
